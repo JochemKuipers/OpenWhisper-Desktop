169af1a2c3d672bcb5db6e8463deb24f7f08eb18
Jochem Kuipers <jochem@kuipers.cc>
2026-04-11 16:59:10 +0200

    add user model

--- openwhisper/user/admin.py
+++ openwhisper/user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from openwhisper.user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'contact_number', 'is_active', 'is_staff', 'is_superuser']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'email', 'contact_number']
+    readonly_fields = ['created_at', 'updated_at']
+    fieldsets = [
+        (None, {'fields': ['username', 'email', 'contact_number']}),
+        ('Profile', {'fields': ['avatar', 'bio', 'birth_date', 'gender', 'location', 'website']}),
+    ]
